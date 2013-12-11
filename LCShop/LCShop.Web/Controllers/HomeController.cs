c1e522ea78ed5559c5c7c8c6045b7ea63af0e8be
luiscunha <luis.cunha.1990@gmail.com>
2013-12-11 21:50:43 +0000

    Application Commit

---
+++ LCShop/LCShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LCShop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "LC Shop - Index";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "LC Shop - About";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "LC Shop - Contacts";
+
+            return View();
+        }
+    }
+}
